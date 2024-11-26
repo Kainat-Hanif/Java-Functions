a11117037072c9002b5ffeacab862de94d28d05f
Kainat Hanif <sp24-bse-005@students.cuisahiwal.edu.pk>
2024-11-26 11:59:51 +0500

    Add files via upload

--- MathFunction.java
+++ MathFunction.java
@@ -2,15 +2,14 @@ import java.util.*;
 public class MathFunction {
     public static void main(String[] args) {
         Scanner s = new Scanner(System.in);
-          System.out.println("Enter 1st number: ");
-          int x = s.nextInt();
-          System.out.println("Enter 2nd number: ");
-          int y = s.nextInt();
-          System.out.println("The maximum between the two numbers is: " + findMax(x, y));
+         
+         System.out.println("Enter a decimal number: ");
+         double a = s.nextDouble();
+             System.out.println("The round of a is: " + Math.round(a));
+             System.out.println("The floor of a is: " + Math.floor(a));
+             System.out.println("The ceil of a is: " + Math.ceil(a));
+             System.out.println("The square of a is: " + Math.sqrt(a));
+             System.out.println("The log of a is: " + Math.log(a));
     }
-    public static int findMax(int a, int b) {
-        int x = Math.max(a, b);
-       return x;
-       
-    }
+    
 }
